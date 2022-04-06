c10bc155260bf26fe1c1f37bd409b31b9c5a344c
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-04-06 15:25:30 -0700

    organizing into Kata Folders + Finished first 7 Kata

---
+++ 8_Kata/sumMixedArray.js
@@ -0,0 +1,28 @@
+/*
+
+
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+
+
+
+
+
+
+*/
+
+
+function sumMix(x){
+
+    let newArr = x.map(Number)
+   
+    
+    let end = newArr.reduce((a,b)=>a+b)
+    
+   return end
+     
+    }
+   
