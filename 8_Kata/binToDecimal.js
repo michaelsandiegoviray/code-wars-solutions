b3b429d9fe97bad16138512a9bd7c5051f82a27e
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-04-20 17:25:02 -0700

    Binary to Decimal file

---
+++ 8_Kata/binToDecimal.js
@@ -0,0 +1,18 @@
+/*
+
+
+
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+
+
+
+*/
+
+
+function binToDec(bin) {
+    let end = parseInt(bin, 2)
+
+    return end
+}
