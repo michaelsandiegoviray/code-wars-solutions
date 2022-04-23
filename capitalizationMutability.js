63b910610c9ed4b671350468a8757165b5fbcdc3
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-04-22 22:36:42 -0700

    Capitalization and mutability

---
+++ capitalizationMutability.js
@@ -0,0 +1,19 @@
+/*
+
+
+function capitalizeWord(word) {
+  let total = word.replace(word[0],word[0].toUpperCase());
+  
+  return total
+;
+}
+
+
+*/
+
+function capitalizeWord(word) {
+    let total = word.replace(word[0], word[0].toUpperCase());
+
+    return total
+        ;
+}
