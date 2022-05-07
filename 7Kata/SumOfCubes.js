24fc371cc4b76854ccee272aff2700a42e9d89b8
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-05-07 11:23:18 -0700

    Sum of Cubes Kata

---
+++ 7Kata/SumOfCubes.js
@@ -0,0 +1,30 @@
+/*
+
+
+Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
+
+Assume that the input n will always be a positive integer.
+
+Examples: (Input --> output)
+
+2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
+3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
+*/
+
+
+function sumCubes(n) {
+
+
+    let yert = []
+    for (let i = 1; i <= n; i++) {
+        yert.push(i)
+    }
+
+    let bert = yert.map(x => Math.pow(x, 3))
+
+    let total = bert.reduce((a, b) => a + b)
+
+    return total
+
+
+}
