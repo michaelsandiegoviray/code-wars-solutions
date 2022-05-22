a7417b929f92b8e24c90c6de8a23c67b326b980e
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-05-22 14:14:37 -0700

    Fake Binary Codewars Problem

---
+++ 8_Kata/fakeBinary.js
@@ -0,0 +1,19 @@
+/*
+
+Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+
+
+
+
+
+*/
+function fakeBin(x) {
+
+    return x.split('').map(i => i >= 5 ? i = 1 : i = 0).join('')
+
+
+
+
+}
