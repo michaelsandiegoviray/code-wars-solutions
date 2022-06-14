e2568662c486c254abb9286747756db519519712
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-06-14 08:44:25 -1000

    Twice as Old

---
+++ 8_Kata/twiceAsOld.js
@@ -0,0 +1,17 @@
+/*
+Your function takes two arguments:
+
+current father's age (years)
+current age of his son (years)
+Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+
+
+
+*/
+function twiceAsOld(d, s) {
+
+
+    return d > 2 * s ? d - (s * 2) : (s * 2) - d
+
+}
