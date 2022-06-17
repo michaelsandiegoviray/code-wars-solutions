00858f18c65a784582b90db66f9d658023ac9314
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-06-16 19:56:45 -1000

    Squaring Every Digit

---
+++ 7Kata/squareEveryDigit.js
@@ -0,0 +1,21 @@
+/*
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer
+
+
+
+
+
+*/
+
+function squareDigits(num) {
+    let newArr = []
+    String(num).split('').forEach((a, b) => newArr.push(Math.pow(a, 2)))
+
+    return Number(newArr.join(''))
+}
+
--- 8_Kata/YouOnlyNeedOne.js
+++ 8_Kata/YouOnlyNeedOne.js
@@ -28,3 +28,5 @@ function check(a, x) {
 
 
 }
+
+
