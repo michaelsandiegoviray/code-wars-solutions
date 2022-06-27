bf806fd06eb86448eafe0fed87db35a8d12f6f14
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-06-27 06:52:29 -1000

    String Templates

---
+++ 8_Kata/stringTemplates.js
@@ -0,0 +1,17 @@
+/*
+
+
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+
+
+
+Previous Form 
+function buildString(...template){
+  return `I like #{template.join(',')}!`;
+}
+
+*/
+
+function buildString(...template) {
+    return `I like ${template.join(', ')}!`;
+}
