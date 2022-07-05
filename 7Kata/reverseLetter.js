aef4025b653657a020091a6dd8398b904fbd96f9
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-07-05 01:47:43 -1000

    Reverse Letter

---
+++ 7Kata/reverseLetter.js
@@ -0,0 +1,26 @@
+/*
+Replaced symbols first then got rid of spaces then did split reverse join 
+
+
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string
+
+
+
+*/
+
+
+function reverseLetter(str) {
+    return str.replace(/[^a-zA-Z ]/g, "").replace(/\s/g, '').split('').reverse().join('')
+
+}
