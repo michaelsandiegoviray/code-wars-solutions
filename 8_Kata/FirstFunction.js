5e4d6890e2fe985658bf7bc725b3ee51bd0dcf08
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-07-11 06:29:06 -1000

    Committing new solution

--- 8_Kata/FirstFunction.js
+++ 8_Kata/FirstFunction.js
@@ -28,13 +28,9 @@ use keyword function to define your function, function name should be helloWorld
 mission 2:
 
 use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
-
 mission 3:
-
 type the console.log() in the next line(don't forget to put the str in the brackets).
-
 When you have finished the work, click "Run Tests" to see if your code is working properly.
-
 In the end, click "Submit" to submit your code pass this kata.
 
 
@@ -43,7 +39,7 @@ In the end, click "Submit" to submit your code pass this kata.
 
 
 function helloWorld() {
-    let str = "Hello World!";
-    console.log(str);
+  let str = "Hello World!";
+  console.log(str);
 }
 helloWorld();
