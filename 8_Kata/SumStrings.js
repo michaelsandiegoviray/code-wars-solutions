f647fb286d1b01773fb156597881bbc7ea1a887f
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-07-12 11:29:51 -0700

    Array Madness

--- 8_Kata/SumStrings.js
+++ 8_Kata/SumStrings.js
@@ -11,6 +11,8 @@ Example: (Input1, Input2 -->Output)
 "-5", "3" --> "-2"
 Notes:
 
+
+
 If either input is an empty string, consider it as zero.
 
 Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
@@ -28,10 +30,9 @@ Inputs and the expected output will never exceed the signed 32-bit integer limit
 
 */
 
-function sumStr(a,b) {
-  
-    return (Number(a)+Number(b)).toString()
-    
-      
-      }
-    
+function sumStr(a, b) {
+
+  return (Number(a) + Number(b)).toString()
+
+
+}
