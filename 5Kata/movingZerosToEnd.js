b9eaf6d17199b241328b9a23586638d996936a17
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-07-13 20:32:12 -0700

    First 5Kyu and File

---
+++ 5Kata/movingZerosToEnd.js
@@ -0,0 +1,25 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+
+
+
+
+*/
+
+
+function moveZeros(arr) {
+
+
+    let regularArr = arr.filter((a, b) => a !== 0)
+
+    let zeros = arr.filter((a, b) => a === 0)
+
+
+    return regularArr.concat(zeros)
+
+
+
+}
