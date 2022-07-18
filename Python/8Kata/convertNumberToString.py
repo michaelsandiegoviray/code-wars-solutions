733622df64322df983a873037d9370576abf6f5e
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-07-17 22:12:01 -0700

    converting Number to String Python

---
+++ Python/8Kata/convertNumberToString.py
@@ -0,0 +1,15 @@
+"""
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
+"""
+
+# Single line comments are it otherwise you need to use multiline docstrings to create a block comment
+
+
+def number_to_string(num):
+
+    return str(num)
