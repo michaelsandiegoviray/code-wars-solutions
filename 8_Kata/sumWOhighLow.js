bd7d5b65f90781de7c2f6efcd9fb312585993311
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-09-03 20:36:16 -0700

    Sum Without Highs and Lows

---
+++ 8_Kata/sumWOhighLow.js
@@ -0,0 +1,22 @@
+/*
+
+
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+
+
+*/
+
+
+function sumArray(array) {
+
+    return decider = array === null || array === undefined || array === [] || array.length < 3 ? 0 : array.sort((a, b) => a - b).filter((element, index) => index > 0 && index < array.length - 1).reduce((a, b) => a + b, 0)
+}
+
