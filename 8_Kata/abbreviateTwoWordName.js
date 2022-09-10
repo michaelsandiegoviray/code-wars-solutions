33d5a442781fc56f1f4dfca30a16b188129a5ad4
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-09-10 00:20:50 -0700

    Added annotation to abbreviate Two Word Name

--- 8_Kata/abbreviateTwoWordName.js
+++ 8_Kata/abbreviateTwoWordName.js
@@ -20,11 +20,11 @@ patrick feeney => P.F
 function abbrevName(name) {
 
     let divided = name.split(' ',);
-
+    // splitting the name into two parts of the array or multiple parts of an array 
     let peace = divided.map(element => element[0].toUpperCase())
-
+    // so we are mapping the divided array and from each element we are taking the 0th character and putting it to uppercase 
     return `${peace[0]}.${peace[1]}`
+    // we are then returning the peace array as a template string to represent initials 
 }
-
 
 
