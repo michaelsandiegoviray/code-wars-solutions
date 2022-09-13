010932f386257a350416524a166392c02824b585
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-09-13 00:30:02 -0700

    annotating map array codewars

--- 8_Kata/beginer-lost-without-a-map.js
+++ 8_Kata/beginer-lost-without-a-map.js
@@ -1,8 +1,10 @@
 function maps(x) {
-    return x.map(n => n * 2);
+  return x.map(n => n * 2);
 }
 
 
 
 
   // Map out a new array that returns a number multiplied by two for each element in the array
+
+// here we are mapping an array and we are multiplying each element by 2 and returning it 
