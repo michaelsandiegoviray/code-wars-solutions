d515914fa4470ff2d424c387527ebbaf3f9917da
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-09-18 00:37:12 -0700

    Convert Number to reversed Array

--- 8_Kata/convertNumberToReversedArray.js
+++ 8_Kata/convertNumberToReversedArray.js
@@ -7,21 +7,17 @@ Example:
 348597 => [7,9,5,8,4,3]
 0 => [0]
 
-
-
-
-
 */
-
-
 function digitize(n) {
- 
+
 
     let array = n.toString().split('').reverse()
-    
-    
-    let result = array.map((n=>Number(n)))
-    
-    
+
+
+    let result = array.map((n => Number(n)))
+
+
     return result
-    }
+}
+
+    // reverse map then return result to change numbers to number 
