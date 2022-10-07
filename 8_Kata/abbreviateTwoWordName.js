7e9fdf883ca876306feec7fa534c606b582674de
michaelsandiegoviray <michaelsandiegoviray@gmail.com>
2022-10-07 00:57:27 -0700

    Abbreviate two WORD NAME

--- 8_Kata/abbreviateTwoWordName.js
+++ 8_Kata/abbreviateTwoWordName.js
@@ -10,21 +10,29 @@ Sam Harris => S.H
 patrick feeney => P.F
 
 
-
-
-
-
-
-
 */
-function abbrevName(name) {
 
-    let divided = name.split(' ',);
-    // splitting the name into two parts of the array or multiple parts of an array 
-    let peace = divided.map(element => element[0].toUpperCase())
-    // so we are mapping the divided array and from each element we are taking the 0th character and putting it to uppercase 
-    return `${peace[0]}.${peace[1]}`
-    // we are then returning the peace array as a template string to represent initials 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function abbrevName(name) {
+
+//     let divided = name.split(' ',);
+//     // splitting the name into two parts of the array or multiple parts of an array 
+//     let peace = divided.map(element => element[0].toUpperCase())
+//     // so we are mapping the divided array and from each element we are taking the 0th character and putting it to uppercase 
+//     return `${peace[0]}.${peace[1]}`
+//     // we are then returning the peace array as a template string to represent initials 
+// }
 
 
